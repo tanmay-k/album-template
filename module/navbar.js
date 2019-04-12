9cd863a72cd8296054170ca6f6f9ba58ddf087e3
Tanmay Kulkarni <tanmaypkulkarni@gmail.com>
2019-04-12 21:01:24 +0530

    Features added: Edit caption and image delete

--- module/navbar.js
+++ module/navbar.js
@@ -20,31 +20,42 @@ class NavbarElement extends HTMLElement {
 			}
 		}
 
-		var getParentUrl = async function()	{
-			var path = '/config.json'
+		var getConfig = async function()	{
+			var path = '/config.json';
 			var archive = new DatArchive(window.location);
-			var config = await archive.readFile(path);
-			return config.parent;
+			var configStr = await archive.readFile(path);
+			console.log(configStr);
+			return JSON.parse(configStr);
 		};
+		var getParentUrl = function()	{
+			//var configStr = getConfigStr();
+			var config = getConfig();
+			console.log(config);
+			return config;
+		};
+
+		var obj = getParentUrl();
+		console.log(obj);
 
 		shadow.innerHTML = `
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/main.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 		<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top justify-content-left">
-			<a class="navbar-brand" href="/">Image Sharing Portal</a>
+			<span class="logo"><a class="navbar-brand" href="/">pixfly</a></span>
 
 			<ul class="navbar-nav">
-				${renderNavLink(`${getParentUrl()}`,'Home')}
+
 			</ul>
 		</nav>
 
 		<div class="contents">
 			<slot></slot>
 		</div>
-		`
+		`;
 	}
 }
 
